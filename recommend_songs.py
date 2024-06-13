78b81bfdbaa9c4602fff2a6a96f08b3c9c82acf8
Yamen-Shb <yamen1929@hotmail.com>
2024-06-13 03:03:06 +0300

    removed limit on track generation

--- recommend_songs.py
+++ recommend_songs.py
@@ -1,6 +1,7 @@
 import audio_features
 import spotify_utils
 import playlist_manager
+import random
 
 
 def chooseSeed(sp, username):
@@ -48,41 +49,58 @@ def recommendBasedOnSeed(sp, username):
     numOfSongs = input("Enter the number of songs you want to generate: ")
     numOfSongs = int(numOfSongs)
 
-    if 1 <= len(songURIs) <= 5:
-        # Use the audio features as seed to get recommendations
-        recommendations = sp.recommendations(seed_tracks=songURIs,
-                                             target_acousticness=audioFeature['acousticness'],
-                                             target_danceability=audioFeature['danceability'],
-                                             target_energy=audioFeature['energy'],
-                                             target_valence=audioFeature['valence'],
-                                             target_tempo=audioFeature['tempo'],
-                                             limit=numOfSongs+1)
-    else:
-        # Use the audio features as seed to get recommendations
-        recommendations = sp.recommendations(seed_tracks=songURIs[0:5],
-                                             target_acousticness=audioFeature['acousticness'],
-                                             target_danceability=audioFeature['danceability'],
-                                             target_energy=audioFeature['energy'],
-                                             target_valence=audioFeature['valence'],
-                                             target_tempo=audioFeature['tempo'],
-                                             limit=numOfSongs+1)
+    recommendations = fetchRecommendations(sp, audioFeature, numOfSongs, songURIs)
 
     # Print or process the recommendations as needed
-    if recommendations['tracks']:
-        print("Recommended Tracks:")
-
+    if recommendations:
         # Extract the track URIs from the recommendations
-        trackURIs = [track['uri'] for track in recommendations['tracks']]
+        trackURIs = [track['uri'] for track in recommendations]
 
-        for track in recommendations['tracks']:
-            if track['uri'] not in songURIs:
-                artists = ', '.join([artist['name'] for artist in track['artists']])
-                print(f"{track['name']} by {artists}")
-
-        # Create a new playlist and add the tracks to it
-        playlistID = playlist_manager.createPlaylist(sp)
-        playlist_manager.addSongsToPlaylist(sp, playlistID, trackURIs)
-
-        print(f"Playlist was created and tracks added successfully.")
+        if trackURIs:
+            # Create a new playlist and add the unique tracks to it
+            playlistID = playlist_manager.createPlaylist(sp)
+            playlist_manager.addSongsToPlaylist(sp, playlistID, trackURIs)
+            print(f"Playlist was created and tracks added successfully.")
+        else:
+            print("No unique tracks to add to the playlist.")
     else:
         print("No recommendations found.")
+
+
+def fetchRecommendations(sp, audioFeature, numOfSongs, songURIs):
+    if len(songURIs) <= 5:
+        usedURIs = songURIs
+    else :
+        usedURIs = random.sample(songURIs, 5)
+    
+    generatedTracks = []
+    numOfTracksToGenerate = numOfSongs
+
+    while len(generatedTracks) < numOfSongs:
+        limit = min(numOfTracksToGenerate, 100)
+
+        recommendations = sp.recommendations(seed_tracks=usedURIs,
+                                             target_acousticness=audioFeature['acousticness'],
+                                             target_danceability=audioFeature['danceability'],
+                                             target_energy=audioFeature['energy'],
+                                             target_valence=audioFeature['valence'],
+                                             target_tempo=audioFeature['tempo'],
+                                             limit=limit)
+
+        newTracks = [track for track in recommendations['tracks'] if track['uri'] not in songURIs and track['uri'] not in [t['uri'] for t in generatedTracks]]
+        generatedTracks.extend(newTracks)
+        
+        # Update the number of tracks to generate in the next loop iteration
+        numOfTracksToGenerate = numOfSongs - len(generatedTracks)
+
+        # Break the loop if no new tracks were added to avoid infinite loop
+        if not newTracks:
+            print("couldn't find any more tracks :c")
+            print("Number of tracks generated: " + len(generatedTracks))
+            break
+
+        # Adjust the seed tracks slightly to reduce overlap
+        if len(songURIs) > 5:
+            usedURIs = random.sample(songURIs, 5)
+
+    return generatedTracks
